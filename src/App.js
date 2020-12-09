492ede8dcbe32c47d833178e139af9087806a2f1
shivgaurav <shivgaurav27@gmail.com>
2020-12-09 09:41:06 +0530

    hooks-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import HooksCounter from './components/HooksCounter';
+import HooksCounterThree from './components/HooksCounterThree';
+import HooksCounterTwo from './components/HooksCounterTwo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* <HooksCounter /> */}
+        {/* <HooksCounterTwo /> */}
+        <HooksCounterThree />
     </div>
   );
 }
---
+++ src/components/HooksCounterTwo.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+function HooksCounterTwo() {
+  const initialCount = 0;
+  const [count, setCount] = useState(0);
+  const incrementFive =()=>{
+      for (let i = 0; i < 5; i++) {
+          setCount(prevCount =>prevCount+1)
+          
+      }
+  } 
+  return (
+    <div>
+      count : {count}
+      <br/>
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={incrementFive}>Increment 5 </button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+}
+
+export default HooksCounterTwo;
